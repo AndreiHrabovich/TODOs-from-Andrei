8fe47f246962fd2ec8b28685496f8d7f708ea92d
Andrei <andrei.hrabovich@gmail.com>
2020-03-04 13:03:33 +0300

    Clean up after deleting SceneDelegate

---
+++ onboarding-screen/onboarding-screen/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  onboarding-screen
+//
+//  Created by A23 on 3/4/20.
+//  Copyright © 2020 A23. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+    
+}
+
---
+++ onboarding-screen/onboarding-screen/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  onboarding-screen
+//
+//  Created by A23 on 3/4/20.
+//  Copyright © 2020 A23. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
