ec1aaefbdcc0da16d602f419e00f19427bced6e1
Andrei <andrei.hrabovich@gmail.com>
2020-03-04 22:37:20 +0300

    Fix VC instantiation + comments + TODOs

--- onboarding-screen/onboarding-screen/Onboarding/OnboardingViewController.swift
+++ onboarding-screen/onboarding-screen/Onboarding/OnboardingViewController.swift
@@ -35,18 +35,15 @@ final class OnboardingViewController: UIViewController {
     }
     
     internal func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-        pageControl.currentPage = Int(targetContentOffset.pointee.x / collectionView.frame.width)
-        // USE collectionView.frame.width because it can be of different value and it directly affects the calculation
-        // probably that's the place to change the button from Skip to Start when reaching the last page
-        handleSkipButtonAppearance()
+        pageControl.currentPage = Int(targetContentOffset.pointee.x / collectionView.frame.width) // - 1
+        handleSkipButtonAppearance() // - 2
     }
     
     // MARK: - IBActions
     
     @IBAction private func skipButtonTapped(_ sender: UIButton) {
-        // move to the main VC
         let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        let mainVC = mainStoryboard.instantiateViewController(identifier: "MainVC") as MainViewController // if that's the only vc in the storyboard, can we do without identifier (Storyboard ID) ?
+        guard let mainVC = mainStoryboard.instantiateViewController(withIdentifier: "MainVC") as? MainViewController else { return } // 3 and 4
         mainVC.modalPresentationStyle = .fullScreen
         
         present(mainVC, animated: true, completion: nil)
@@ -57,9 +54,9 @@ final class OnboardingViewController: UIViewController {
     
     private func setupUI() {
         collectionView.register(UINib(nibName: cellID, bundle: nil), forCellWithReuseIdentifier: cellID)
-        collectionView.delegate = self // is needed anyways (without it the cells don't scale properly)
+        collectionView.delegate = self // - 5
         collectionView.dataSource = self
-        collectionView.isPagingEnabled = true // to enable paging
+        collectionView.isPagingEnabled = true
         layout.scrollDirection = .horizontal
         
         pageControl.numberOfPages = colors.count
@@ -68,7 +65,7 @@ final class OnboardingViewController: UIViewController {
     }
     
     private func handleSkipButtonAppearance () {
-        if pageControl.currentPage == (colors.count - 1) { // because the array starts from 0
+        if pageControl.currentPage == (colors.count - 1) { // - 6
             skipButton.setTitle("Start", for: .normal)
         } else {
             skipButton.setTitle("Skip", for: .normal)
@@ -90,6 +87,7 @@ extension OnboardingViewController: UICollectionViewDataSource {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as? PageCell else { return UICollectionViewCell() }
         let color = colors[indexPath.item]
         cell.backgroundColor = color
+        #warning("TODO - configure cell in the Page Cell")
         return cell
     }
 }
@@ -108,10 +106,23 @@ extension OnboardingViewController: UICollectionViewDelegateFlowLayout {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0 // need to overrride this bacause it will add 5 or 10 by default
+        return 0 // - 7
     }
 }
 
 #warning("TODO - to figure out how to avoid showing this thing every time, should work only for the first time")
 #warning("TODO - to find out whether it's possible to use the page control to navigate the onboarding screen")
 #warning("TODO - a separate branch with comments left")
+#warning("To deploy to a real device wirelessly, make sure the Mac and the iOS device are connected to the same network, otherwise use TestFlight")
+
+/*
+ Comments:
+ 
+ 1 - USE collectionView.frame.width because it can be of different value and it directly affects the calculation
+ 2 - Probably that's the place to change the button from Skip to Start when reaching the last page
+ 3 - STORYBOARD.instantiateViewController(identifier: "MainVC") - available only in iOS13
+ 4 - If that's the only vc in the storyboard, can we do without identifier (Storyboard ID) ?
+ 5 - The delegate is needed anyways (without it the cells don't scale properly)
+ 6 - Because the array starts from 0
+ 7 - Returning 0 is need to overrride this bacause it will add 5 or 10 by default
+ */
