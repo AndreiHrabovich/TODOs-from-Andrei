dc2c41ebd759da66877c823564cf045d455daf3e
Andrei <andrei.hrabovich@gmail.com>
2020-03-04 15:02:40 +0300

    Add starter setup

--- onboarding-screen/onboarding-screen/Onboarding/OnboardingViewController.swift
+++ onboarding-screen/onboarding-screen/Onboarding/OnboardingViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 final class OnboardingViewController: UIViewController {
+    // MARK: - Properties
+    
+    #warning("TODO - to use R.swift instead")
+    private let cellID = "PageCell"
+    private let colors: [UIColor] = [.green, .blue, .yellow, .orange, .red]
+    
+    // MARK: - IBOutlets
+    
     @IBOutlet private weak var collectionView: UICollectionView!
     @IBOutlet private weak var layout: UICollectionViewFlowLayout!
     
-    let cellID = "PageCell"
-    
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    private func setupCOllectionView() {
+//        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.isPagingEnabled = true // to enable paging
+        layout.scrollDirection = .horizontal
+    }
+}
+
+#warning("TODO - to extract delegate and data source to a standalone entity")
+// MARK: - UICollectionViewDataSource
+
+extension OnboardingViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as? PageCell else { return UICollectionViewCell() }
+        let color = colors[indexPath.item]
+        cell.backgroundColor = color
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension OnboardingViewController: UICollectionViewDelegateFlowLayout {
 }
