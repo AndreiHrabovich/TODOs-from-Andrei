7ba47ac15aefb0bbee38e9f109afdaf1b3f520d0
Andrei <andrei.hrabovich@gmail.com>
2020-03-04 14:45:22 +0300

    Add PageCell + XIB

---
+++ onboarding-screen/onboarding-screen/Onboarding/PageCell/PageCell.swift
@@ -0,0 +1,18 @@
+//
+//  PageCell.swift
+//  onboarding-screen
+//
+//  Created by A23 on 3/4/20.
+//  Copyright © 2020 A23. All rights reserved.
+//
+
+import UIKit
+
+class PageCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
