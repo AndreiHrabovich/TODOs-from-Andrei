2377b85f030b2936a1e79ef822be94c1ca9f7371
Andrei <andrei.hrabovich@gmail.com>
2020-03-06 13:04:32 +0300

    Restructuring

---
+++ onboarding-screen/onboarding-screen/Application/Assembler.swift
@@ -0,0 +1,20 @@
+//
+//  Assembler.swift
+//  onboarding-screen
+//
+//  Created by A23 on 3/6/20.
+//  Copyright © 2020 A23. All rights reserved.
+//
+
+import UIKit
+
+final class Assembler {
+    static func assembleRootVC(withFlag flag: Bool) -> UIViewController {
+        let onboardingVC = UIStoryboard(name: "Onboarding", bundle: nil)
+            .instantiateViewController(withIdentifier: "OnboardingVC")
+        let mainVC = UIStoryboard(name: "Main", bundle: nil)
+            .instantiateViewController(withIdentifier: "MainVC")
+        
+        return flag ? onboardingVC : mainVC
+    }
+}
